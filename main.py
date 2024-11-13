4362679d400263644d824bd58ea260b39575bcfd
Kamivision <54725955+Kamivision@users.noreply.github.com>
2024-11-13 15:39:53 -0600

    update

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import kivy
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
-from kivy.uix.widget import Widget
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import ObjectProperty
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
+
 
 
 
